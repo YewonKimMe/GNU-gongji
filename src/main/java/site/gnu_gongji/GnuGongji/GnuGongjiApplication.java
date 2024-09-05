ec9da7458b419ca64749cd202609b33005f5ab94
YewonKimMe <kimyewon030428@gmial.com>
2024-09-05 12:47:36 +0900

    initial commit

---
+++ src/main/java/site/gnu_gongji/GnuGongji/GnuGongjiApplication.java
@@ -0,0 +1,13 @@
+package site.gnu_gongji.GnuGongji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnuGongjiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GnuGongjiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/gnu_gongji/GnuGongji/GnuGongjiApplicationTests.java
@@ -0,0 +1,13 @@
+package site.gnu_gongji.GnuGongji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GnuGongjiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
