ddc2b3972fc354e3b620c774da3d2d5f9d3c9677
YewonKimMe <kimyewon030428@gmial.com>
2024-09-14 16:40:50 +0900

    Department

---
+++ src/main/java/site/gnu_gongji/GnuGongji/entity/Department.java
@@ -0,0 +1,31 @@
+package site.gnu_gongji.GnuGongji.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "department_id")
+    private Long departmentId;
+
+    @Column(name = "department_ko")
+    private String departmentKo;
+
+    @Column(name = "department_eng")
+    private String departmentEng;
+
+    @OneToMany(mappedBy = "department")
+    private List<UserSubDepartment> userSubDepartmentList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "department")
+    private List<DepartmentNoticeInfo> departmentNoticeInfoList = new ArrayList<>();
+}
