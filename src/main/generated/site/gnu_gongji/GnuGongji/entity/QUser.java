55c1693cc0b2155f24bc9cb6dad69997cedf967c
YewonKimMe <kimyewon030428@gmial.com>
2024-09-15 22:35:52 +0900

    QUser

---
+++ src/main/generated/site/gnu_gongji/GnuGongji/entity/QUser.java
@@ -0,0 +1,42 @@
+package site.gnu_gongji.GnuGongji.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QUser is a Querydsl query type for User
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QUser extends EntityPathBase<User> {
+
+    private static final long serialVersionUID = -1980425504L;
+
+    public static final QUser user = new QUser("user");
+
+    public final SetPath<UserSub, QUserSub> subList = this.<UserSub, QUserSub>createSet("subList", UserSub.class, QUserSub.class, PathInits.DIRECT2);
+
+    public final StringPath userEmail = createString("userEmail");
+
+    public final NumberPath<Long> userId = createNumber("userId", Long.class);
+
+    public QUser(String variable) {
+        super(User.class, forVariable(variable));
+    }
+
+    public QUser(Path<? extends User> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QUser(PathMetadata metadata) {
+        super(User.class, metadata);
+    }
+
+}
+
