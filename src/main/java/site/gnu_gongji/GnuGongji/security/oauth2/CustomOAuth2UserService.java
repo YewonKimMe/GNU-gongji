13059dfc9e3050c71882cc57726c25fc68a4c821
YewonKimMe <kimyewon030428@gmial.com>
2024-09-15 22:38:21 +0900

    CustomOAuth2UserService

---
+++ src/main/java/site/gnu_gongji/GnuGongji/security/oauth2/CustomOAuth2UserService.java
@@ -0,0 +1,48 @@
+package site.gnu_gongji.GnuGongji.security.oauth2;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import site.gnu_gongji.GnuGongji.exception.OAuth2AuthenticationProcessingException;
+
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        try {
+            return processOAut2User(userRequest, oAuth2User);
+        } catch (AuthenticationException ex) {
+            throw ex;
+        } catch (Exception e) {
+            throw new InternalAuthenticationServiceException(e.getMessage(), e.getCause());
+        }
+    }
+
+    private OAuth2User processOAut2User(OAuth2UserRequest userRequest, OAuth2User oAuth2User) {
+
+        // 클라이언트 등록 정보 획득
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+        // 엑세스 토큰 획득
+        String accessToken = userRequest.getAccessToken().getTokenValue();
+
+        // 등록 정보, 엑세스 토큰 기반으로 OAuth2 제공자 별 UserInfo 가져오기
+        OAuth2UserInfo oAuth2UserInfo = OAuth2UserInfoFactory.getOAtu2UserInfo(registrationId, accessToken, oAuth2User.getAttributes());
+
+        // OAuth2UserInfo field value validation
+        if (!StringUtils.hasText(oAuth2UserInfo.getEmail())) {
+            throw new OAuth2AuthenticationProcessingException("Email not found from OAuth2 provider");
+        }
+        return new OAuth2UserPrincipal(oAuth2UserInfo);
+    }
+}
