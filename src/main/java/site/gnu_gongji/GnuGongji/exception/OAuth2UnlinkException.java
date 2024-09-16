3adf40145c3ce5498e36854bd216f697dc1ab8de
YewonKimMe <kimyewon030428@gmial.com>
2024-09-16 18:37:27 +0900

    OAuth2UnlinkException

---
+++ src/main/java/site/gnu_gongji/GnuGongji/exception/OAuth2UnlinkException.java
@@ -0,0 +1,7 @@
+package site.gnu_gongji.GnuGongji.exception;
+
+public class OAuth2UnlinkException extends RuntimeException {
+    public OAuth2UnlinkException(String message) {
+        super(message);
+    }
+}
