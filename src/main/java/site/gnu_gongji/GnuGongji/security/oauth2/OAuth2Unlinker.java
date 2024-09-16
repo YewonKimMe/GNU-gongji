7496199d72ee26bd26fa16fdd5313cdd5ef6f969
YewonKimMe <kimyewon030428@gmial.com>
2024-09-17 02:12:33 +0900

    OAuth2User parameter added

--- src/main/java/site/gnu_gongji/GnuGongji/security/oauth2/OAuth2Unlinker.java
+++ src/main/java/site/gnu_gongji/GnuGongji/security/oauth2/OAuth2Unlinker.java
@@ -1,7 +1,9 @@
 package site.gnu_gongji.GnuGongji.security.oauth2;
 
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
 public interface OAuth2Unlinker {
 
-    void unlink(String accessToken);
+    void unlink(String accessToken, OAuth2User oAuth2User);
 
 }
