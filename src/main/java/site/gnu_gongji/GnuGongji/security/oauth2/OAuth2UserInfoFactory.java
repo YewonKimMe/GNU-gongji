778b0a13f122e67a140dfcde73818993f087d656
YewonKimMe <kimyewon030428@gmial.com>
2024-09-17 02:54:16 +0900

    GitHubAuth2UserInfo added

--- src/main/java/site/gnu_gongji/GnuGongji/security/oauth2/OAuth2UserInfoFactory.java
+++ src/main/java/site/gnu_gongji/GnuGongji/security/oauth2/OAuth2UserInfoFactory.java
@@ -1,14 +1,21 @@
 package site.gnu_gongji.GnuGongji.security.oauth2;
 
+import lombok.extern.slf4j.Slf4j;
 import site.gnu_gongji.GnuGongji.exception.OAuth2AuthenticationProcessingException;
 
 import java.util.Map;
 
+@Slf4j
 public class OAuth2UserInfoFactory {
 
     public static OAuth2UserInfo getOAtu2UserInfo(String registrationId, String accessToken, Map<String, Object> attributes) {
 
-        if (OAuth2Provider.KAKAO.getRegistrationId().equals(registrationId)) {
+        log.debug("attributes={}", attributes);
+
+        if (OAuth2Provider.GITHUB.getRegistrationId().equals(registrationId)) {
+            // GitHub OAuth2 UserInfo
+            return new GithubOAuth2UserInfo(accessToken, attributes);
+        } else if (OAuth2Provider.KAKAO.getRegistrationId().equals(registrationId)) {
             // Kakao OAuth2 UserInfo
             return new KakaoOAuth2UserInfo(accessToken, attributes);
         } else if (OAuth2Provider.NAVER.getRegistrationId().equals(registrationId)) {
