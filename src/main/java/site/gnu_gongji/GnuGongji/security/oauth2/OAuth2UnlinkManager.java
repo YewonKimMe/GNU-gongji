27c28c9cb2c562753cc1d39541a770f73edbd506
YewonKimMe <kimyewon030428@gmial.com>
2024-09-17 02:12:41 +0900

    OAuth2User parameter added

--- src/main/java/site/gnu_gongji/GnuGongji/security/oauth2/OAuth2UnlinkManager.java
+++ src/main/java/site/gnu_gongji/GnuGongji/security/oauth2/OAuth2UnlinkManager.java
@@ -1,6 +1,7 @@
 package site.gnu_gongji.GnuGongji.security.oauth2;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
 import site.gnu_gongji.GnuGongji.exception.OAuth2UnlinkException;
 
@@ -12,12 +13,12 @@ public class OAuth2UnlinkManager {
 
     private final NaverOAuth2Unlinker naverOAuth2Unlinker;
 
-    public void processUnlink(OAuth2Provider provider, String accessToken) {
+    public void processUnlink(OAuth2Provider provider, String accessToken, OAuth2User oAuth2User) {
 
         switch (provider) {
-            case KAKAO -> kakaoOAuth2Unlinker.unlink(accessToken);
+            case KAKAO -> kakaoOAuth2Unlinker.unlink(accessToken, oAuth2User);
 
-            case NAVER -> naverOAuth2Unlinker.unlink(accessToken);
+            case NAVER -> naverOAuth2Unlinker.unlink(accessToken, oAuth2User);
 
             default -> throw new OAuth2UnlinkException("Unlink to " + provider.getRegistrationId() + " is not support.");
         }
