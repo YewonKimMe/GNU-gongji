0af1cc8cd4e065bab8921be798ace28917cf41c6
YewonKimMe <kimyewon030428@gmial.com>
2024-09-18 04:01:20 +0900

    set oauth field

--- src/main/java/site/gnu_gongji/GnuGongji/entity/User.java
+++ src/main/java/site/gnu_gongji/GnuGongji/entity/User.java
@@ -2,15 +2,19 @@ package site.gnu_gongji.GnuGongji.entity;
 
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
+import java.sql.Timestamp;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 @Entity
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "user")
 public class User {
 
@@ -19,10 +23,28 @@ public class User {
     @Column(name = "user_id")
     private Long userId;
 
-    @Column(name = "user_email")
+    @Column(name = "email")
     private String userEmail;
 
-    @OneToMany(mappedBy = "user")
+    @Column(name = "is_oauth2")
+    private boolean isOAuth2;
+
+    @Column(name = "oauth2_provider")
+    private String oauth2Provider;
+
+    @Column(name = "oauth2_id")
+    private String oauth2Id;
+
+    @Column(name = "refresh_token", length = 1500)
+    private String refreshToken;
+
+    @Column(name = "create_date")
+    private Timestamp createDate;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Authority> authorities = new HashSet<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<UserSub> subList = new LinkedHashSet<>();
 
 }
