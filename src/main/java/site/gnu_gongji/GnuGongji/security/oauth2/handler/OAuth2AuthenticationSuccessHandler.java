fe9f2b4a44b79f473ac833f2a2c800458686e4b1
YewonKimMe <kimyewon030428@gmial.com>
2024-09-18 17:28:55 +0900

    LOGIN, UNLINK function

--- src/main/java/site/gnu_gongji/GnuGongji/security/oauth2/handler/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/site/gnu_gongji/GnuGongji/security/oauth2/handler/OAuth2AuthenticationSuccessHandler.java
@@ -1,22 +1,28 @@
 package site.gnu_gongji.GnuGongji.security.oauth2.handler;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
+import site.gnu_gongji.GnuGongji.dto.UserCreateDto;
 import site.gnu_gongji.GnuGongji.security.TokenManger;
 import site.gnu_gongji.GnuGongji.security.oauth2.*;
 import site.gnu_gongji.GnuGongji.security.oauth2.enums.TokenDurationTime;
 import site.gnu_gongji.GnuGongji.security.oauth2.enums.TokenType;
+import site.gnu_gongji.GnuGongji.service.UserManageService;
 import site.gnu_gongji.GnuGongji.tool.CookieUtils;
 
 import java.io.IOException;
+import java.util.Base64;
 import java.util.Optional;
 
 import static site.gnu_gongji.GnuGongji.security.oauth2.OAuth2AuthorizationRequestCookieRepository.*;
@@ -32,19 +38,29 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
 
     private final TokenManger tokenManger;
 
+    private final UserManageService userManageService;
+
+    private final String error = "error";
+    private final String process = "process";
+
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
-        String targetUrl = determineTargetUrl(request, response, authentication);
-
         if (response.isCommitted()) { // 응답 객체가 이미 커밋된 경우
-            log.debug("Response has already been committed, Unable to redirect to targetUrl: " + targetUrl);
+            log.debug("Response has already been committed, Unable to redirect to targetUrl");
             return;
         }
+
+        String targetUrl = determineTargetUrl(request, response, authentication);
+
         customClearAuthenticationAttributes(request, response); // 요청 객체에 있는 인증 관련 요소를 제거하고 쿠키도 제거
 
-        getRedirectStrategy().sendRedirect(request, response, targetUrl);
-
+        if (targetUrl.contains(error)) {
+            response.sendError(HttpStatus.BAD_REQUEST.value(), "Requested OAuth2 processing failed.");
+        } else {
+            getRedirectStrategy().sendRedirect(request, response, targetUrl);
+        }
     }
 
     @Override
@@ -58,55 +74,85 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
 
         OAuth2UserPrincipal oAuth2UserPrincipal = getOAuth2UserPrincipal(authentication);
 
+
         if (null == oAuth2UserPrincipal) {
             return UriComponentsBuilder.fromUriString(targetUrl)
-                    .queryParam("error", "LoginFailed")
+                    .queryParam(error, "LoginProcessFailed")
                     .build().toUriString();
         }
 
         return switch (mode.toLowerCase()) {
-            case "login" -> {
-                log.info("email={}, name={}, nickname={}, accessToken={}", oAuth2UserPrincipal.getUserInfo().getEmail(),
-                        oAuth2UserPrincipal.getUserInfo().getName(),
-                        oAuth2UserPrincipal.getUserInfo().getNickname(),
-                        oAuth2UserPrincipal.getUserInfo().getAccessToken()
-                );
 
-                // 토큰 생성, ROLE 추가
-                String accessToken = tokenManger.createJwtToken(authentication, oAuth2UserPrincipal, TokenType.ACCESS, TokenDurationTime.ACCESS);
-                String refreshToken = tokenManger.createJwtToken(authentication, oAuth2UserPrincipal, TokenType.REFRESH, TokenDurationTime.REFRESH);
+            case "login" -> handleLoginProcess(authentication, oAuth2UserPrincipal, targetUrl);
 
-                // TODO: UserInfo DB Save
+            case "unlink" -> handleUnlinkProcess(oAuth2UserPrincipal, targetUrl);
 
-                // TODO: RefreshToken DB Save
-
-                yield UriComponentsBuilder.fromUriString(targetUrl)
-                        .queryParam("accessToken", accessToken)
-                        //.queryParam("refresh_token", refreshToken)
-                        .build().toUriString();
-            }
-            case "unlink" -> {
-                String accessToken = oAuth2UserPrincipal.getUserInfo().getAccessToken();
-                OAuth2Provider provider = oAuth2UserPrincipal.getUserInfo().getProvider();
-
-                // TODO: DB 유저 정보 제거
-
-                // TODO: RefreshToken 제거
-                oAuth2UnlinkManager.processUnlink(provider, accessToken, oAuth2UserPrincipal);
-
-                yield UriComponentsBuilder.fromUriString(targetUrl)
-                        .build()
-                        .toUriString();
-            }
-            default -> {
-                log.debug("[OAuth2 Login Failed] mode={}, ", mode);
-
-                yield UriComponentsBuilder.fromUriString(targetUrl)
-                        .queryParam("error", "LoginProcessFailed")
-                        .build()
-                        .toUriString();
-            }
+            default -> handleDefaultProcess(mode, targetUrl);
         };
+    }
+
+    private String handleLoginProcess(Authentication authentication, OAuth2UserPrincipal oAuth2UserPrincipal, String targetUrl) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        log.info("email={}, name={}, nickname={}, accessToken={}", oAuth2UserPrincipal.getUserInfo().getEmail(),
+                oAuth2UserPrincipal.getUserInfo().getName(),
+                oAuth2UserPrincipal.getUserInfo().getNickname(),
+                oAuth2UserPrincipal.getUserInfo().getAccessToken()
+        );
+
+        // 토큰 생성, ROLE 추가
+        String accessToken = tokenManger.createJwtToken(authentication, oAuth2UserPrincipal, TokenType.ACCESS, TokenDurationTime.ACCESS);
+        String refreshToken = tokenManger.createJwtToken(authentication, oAuth2UserPrincipal, TokenType.REFRESH, TokenDurationTime.REFRESH);
+
+
+        // Duplication check
+        if (userManageService.isUserAlreadyExist(oAuth2UserPrincipal.getUserInfo().getEmail())) {
+            return UriComponentsBuilder.fromUriString(targetUrl)
+                    .queryParam(process, "login")
+                    .queryParam("exist", "true")
+                    .queryParam("accessToken", accessToken)
+                    .build().toUriString();
+        }
+
+        // UserInfo DB Save, RefreshToken DB Save
+        UserCreateDto oAuth2User = userManageService.createOAuth2User(oAuth2UserPrincipal, refreshToken);
+
+        try {
+            return UriComponentsBuilder.fromUriString(targetUrl)
+                    .queryParam("exist", "false")
+                    .queryParam("accessToken", accessToken)
+                    .queryParam("user", Base64.getEncoder().encodeToString(objectMapper.writeValueAsString(oAuth2User).getBytes()))
+                    .build().toUriString();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String handleUnlinkProcess(OAuth2UserPrincipal oAuth2UserPrincipal, String targetUrl) {
+        String accessToken = oAuth2UserPrincipal.getUserInfo().getAccessToken();
+        OAuth2Provider provider = oAuth2UserPrincipal.getUserInfo().getProvider();
+
+        // UNLINK
+        oAuth2UnlinkManager.processUnlink(provider, accessToken, oAuth2UserPrincipal);
+
+        // DB 유저 정보 제거
+        userManageService.deleteOAuth2User(oAuth2UserPrincipal.getUserInfo().getId(), provider);
+
+        return UriComponentsBuilder.fromUriString(targetUrl)
+                .queryParam(process, "unlink")
+                .queryParam("status", "success")
+                .build()
+                .toUriString();
+    }
+
+    private String handleDefaultProcess(String mode, String targetUrl) {
+        log.debug("[OAuth2 Login Failed] mode={}, ", mode);
+
+        return UriComponentsBuilder.fromUriString(targetUrl)
+                .queryParam(error, "LoginProcessFailed")
+                .build()
+                .toUriString();
     }
 
     private OAuth2UserPrincipal getOAuth2UserPrincipal(Authentication authentication) {
