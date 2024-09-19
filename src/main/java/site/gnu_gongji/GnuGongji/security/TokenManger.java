929e3e0899d0c4d7ce31251fad6a8903963479c3
YewonKimMe <kimyewon030428@gmial.com>
2024-09-19 20:40:02 +0900

    refresh token 재발급 로직 포함

--- src/main/java/site/gnu_gongji/GnuGongji/security/TokenManger.java
+++ src/main/java/site/gnu_gongji/GnuGongji/security/TokenManger.java
@@ -15,53 +15,137 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import site.gnu_gongji.GnuGongji.security.oauth2.OAuth2UserPrincipal;
 import site.gnu_gongji.GnuGongji.security.oauth2.enums.Role;
 import site.gnu_gongji.GnuGongji.security.oauth2.enums.TokenDurationTime;
 import site.gnu_gongji.GnuGongji.security.oauth2.enums.TokenType;
+import site.gnu_gongji.GnuGongji.service.UserManageService;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
-import java.security.Key;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.*;
+
+import static site.gnu_gongji.GnuGongji.security.oauth2.enums.JWTClaimKey.*;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class TokenManger {
 
+    private final UserManageService userManageService;
+
     private static final long ACCESS_TOKEN_EXPIRE_TIME_IN_MILLISECONDS_VALUE = 1000 * 60 * 30; // 30분
     private static final long ACCESS_TOKEN_EXPIRE_TIME_IN_TIME = 3; // 3시간
 
     @Value("${mycustom.jwt.secret}")
     private String jwtSecret;
-    private Key key;
 
-    public boolean validateJwtToken(String token) {
+    public boolean validateJwtToken(String token, TokenType tokenType) {
         try {
             // 기존 비밀키 생성
             SecretKey existingSecretKey = getSecretKey(jwtSecret);
 
+            // jwt 검증
             Jwts.parser()
                     .verifyWith(existingSecretKey)
                     .build()
                     .parseSignedClaims(token);
 
             return true;
+
         } catch (UnsupportedJwtException | MalformedJwtException exception) {
             log.error("JWT is not valid");
         } catch (SignatureException exception) {
             log.error("JWT signature validation fails");
         } catch (ExpiredJwtException exception) {
+
             log.error("JWT is expired");
+
+            // jwt 만료 상황
+            if (tokenType == TokenType.ACCESS) {
+                return checkRefreshToken(token, jwtSecret);
+            } else if (tokenType == TokenType.REFRESH) {
+
+                Claims claim = getClaim(token, jwtSecret);
+
+                String provider = String.valueOf(claim.get(PROVIDER.getClaimKey()));
+                String oauth2Id = String.valueOf(claim.get(OAUTH2_ID.getClaimKey()));
+                String username = String.valueOf(claim.get(USERNAME.getClaimKey()));
+                String authorities = String.valueOf(claim.get(AUTHORITIES.getClaimKey()));
+
+                // 토큰 재생성, 사용자 정보에 저장
+                String newRefreshToken = createRefreshToken(
+                        TokenType.REFRESH,
+                        TokenDurationTime.REFRESH,
+                        claim.getSubject(),
+                        provider,
+                        oauth2Id,
+                        username,
+                        authorities);
+
+                userManageService.updateRefreshToken(oauth2Id, provider, newRefreshToken);
+                // TODO 새 ACCESS TOKEN 을 클라이언트로 어떻게 반환할건지
+                return true;
+            }
+
         } catch (IllegalArgumentException exception) {
             log.error("JWT is null or empty or only whitespace");
         } catch (Exception exception) {
             log.error("JWT validation fails", exception);
         }
         return false;
+    }
+
+    private boolean checkRefreshToken(String invalidAccessJwt, String jwtSecret) {
+
+        Claims claim = getClaim(invalidAccessJwt, jwtSecret);
+
+        String oauth2Id = String.valueOf(claim.get(OAUTH2_ID.getClaimKey()));
+        String oauth2Provider = String.valueOf(claim.get(PROVIDER.getClaimKey()));
+
+        if (!StringUtils.hasText(oauth2Id) || !StringUtils.hasText(oauth2Provider)) {
+            return false;
+        }
+        Optional<site.gnu_gongji.GnuGongji.entity.User> findUserOpt = userManageService.findOAuth2User(oauth2Id, oauth2Provider);
+
+        if (findUserOpt.isEmpty()) return false;
+
+        site.gnu_gongji.GnuGongji.entity.User user = findUserOpt.get();
+
+        String refreshToken = user.getRefreshToken();
+
+        return validateJwtToken(refreshToken, TokenType.REFRESH);
+    }
+
+    private String createRefreshToken(TokenType tokenType, TokenDurationTime tokenDurationTime, String subject, String provider, String oauth2Id, String username, String authorities) {
+        SecretKey secretKey = getSecretKey(jwtSecret);
+
+        Instant expirationTime = Instant.now().plus(Duration.ofHours(tokenDurationTime.getTime()));
+
+        return Jwts.builder()
+                .issuer(ISSUER.getClaimKey())
+                .subject(subject)
+                .expiration(Date.from(expirationTime))
+                .claim(TYPE.getClaimKey(), tokenType.getTokenName())
+                .claim(PROVIDER.getClaimKey(), provider)
+                .claim(OAUTH2_ID.getClaimKey(), oauth2Id)
+                .claim(USERNAME.getClaimKey(), username)
+                .claim(AUTHORITIES.getClaimKey(), authorities)
+                .signWith(secretKey)
+                .compact();
+    }
+
+    private Claims getClaim(String jwtToken, String jwtSecret) {
+        SecretKey secretKey = getSecretKey(jwtSecret);
+
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(jwtToken)
+                .getPayload();
     }
 
     public String createJwtToken(Authentication authentication, OAuth2User oAuth2User, TokenType tokenType, TokenDurationTime tokenDurationTime) {
@@ -93,14 +177,14 @@ public class TokenManger {
         Instant expirationTime = Instant.now().plus(Duration.ofHours(tokenDurationTime.getTime()));
 
         return Jwts.builder()
-                .issuer("GNU-GONGJI")
+                .issuer(ISSUER.getClaimKey())
                 .subject(authentication.getName())
                 .expiration(Date.from(expirationTime))
-                .claim("type", tokenType.getTokenName())
-                .claim("provider", oAuth2AuthenticationToken != null ? oAuth2AuthenticationToken.getAuthorizedClientRegistrationId() : null)
-                .claim("id", oAuth2UserPrincipal != null ? oAuth2UserPrincipal.getUserInfo().getId() : null)
-                .claim("username", authentication.getName())
-                .claim("authorities", populateAuthorities(authentication.getAuthorities()))
+                .claim(TYPE.getClaimKey(), tokenType.getTokenName())
+                .claim(PROVIDER.getClaimKey(), oAuth2AuthenticationToken != null ? oAuth2AuthenticationToken.getAuthorizedClientRegistrationId() : null)
+                .claim(OAUTH2_ID.getClaimKey(), oAuth2UserPrincipal != null ? oAuth2UserPrincipal.getUserInfo().getId() : null)
+                .claim(USERNAME.getClaimKey(), authentication.getName())
+                .claim(AUTHORITIES.getClaimKey(), populateAuthorities(authentication.getAuthorities()))
                 .signWith(secretKey)
                 .compact();
     }
@@ -114,8 +198,8 @@ public class TokenManger {
                 .build()
                 .parseSignedClaims(jwtToken)
                 .getPayload();
-        String username = String.valueOf(claims.get("username"));
-        String authorities = String.valueOf(claims.get("authorities"));
+        String username = String.valueOf(claims.get(USERNAME.getClaimKey()));
+        String authorities = String.valueOf(claims.get(AUTHORITIES.getClaimKey()));
         List<GrantedAuthority> authoritiesList = AuthorityUtils.commaSeparatedStringToAuthorityList(authorities);
         UserDetails user = new User(username, "", authoritiesList);
 
