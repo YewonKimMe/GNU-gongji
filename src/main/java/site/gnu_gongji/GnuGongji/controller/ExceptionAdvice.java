9aed852903a9e37d9f359d3bff60f85f64d55944
YewonKimMe <kimyewon030428@gmial.com>
2024-09-21 16:18:46 +0900

    ExceptionAdvice

---
+++ src/main/java/site/gnu_gongji/GnuGongji/controller/ExceptionAdvice.java
@@ -0,0 +1,24 @@
+package site.gnu_gongji.GnuGongji.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import site.gnu_gongji.GnuGongji.dto.response.FailResultAndMessage;
+import site.gnu_gongji.GnuGongji.dto.response.ResultAndMessage;
+
+@RestControllerAdvice
+@Slf4j
+public class ExceptionAdvice {
+
+    // JWT Filter 예외 처리
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ResultAndMessage> handleBadCredentialException(BadCredentialsException e) {
+
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(new FailResultAndMessage(HttpStatus.UNAUTHORIZED.getReasonPhrase(), e.getMessage()));
+    }
+}
