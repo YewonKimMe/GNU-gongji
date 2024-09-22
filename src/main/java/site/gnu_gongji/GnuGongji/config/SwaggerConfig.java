d1b22bae85110389bf866abaadc4abc9d66b2249
YewonKimMe <kimyewon030428@gmial.com>
2024-09-22 20:48:36 +0900

    SwaggerConfig

---
+++ src/main/java/site/gnu_gongji/GnuGongji/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package site.gnu_gongji.GnuGongji.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER).name("Authorization"))
+                        .addSecuritySchemes("basicAuth",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                                        .scheme("basic")))
+                .info(apiInformation())
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .addSecurityItem(new SecurityRequirement().addList("basicAuth"));
+    }
+
+    private Info apiInformation() {
+        return new Info()
+                .title("GNU GONGJI API")
+                .description("GNU-GONGJI REST API V1.0.0")
+                .version("1.0.0");
+    }
+}
