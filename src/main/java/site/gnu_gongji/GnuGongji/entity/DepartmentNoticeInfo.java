a38314ae8ba166c43f340cf2d72368f8fc9fea11
YewonKimMe <kimyewon030428@gmial.com>
2024-09-22 20:49:31 +0900

    @ToString

--- src/main/java/site/gnu_gongji/GnuGongji/entity/DepartmentNoticeInfo.java
+++ src/main/java/site/gnu_gongji/GnuGongji/entity/DepartmentNoticeInfo.java
@@ -3,10 +3,12 @@ package site.gnu_gongji.GnuGongji.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
 @Setter
+@ToString
 @Table(name = "department_nottice_info")
 public class DepartmentNoticeInfo {
 
