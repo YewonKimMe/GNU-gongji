3b302c65b1c6a47fbbce8b3642efad6232b4b38b
YewonKimMe <kimyewon030428@gmial.com>
2024-09-22 12:57:52 +0900

    redirectUrl

--- src/main/java/site/gnu_gongji/GnuGongji/security/oauth2/OAuth2AuthorizationRequestCookieRepository.java
+++ src/main/java/site/gnu_gongji/GnuGongji/security/oauth2/OAuth2AuthorizationRequestCookieRepository.java
@@ -14,7 +14,7 @@ import site.gnu_gongji.GnuGongji.tool.CookieUtils;
 public class OAuth2AuthorizationRequestCookieRepository implements AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
 
     public static final String OAUTH2_AUTH_REQUEST_COOKIE_NAME = "oauth2AuthRequest";
-    public static final String REDIRECT_URI_PARAMETER_COOKIE_NAME = "redirectUri";
+    public static final String REDIRECT_URI_PARAMETER_COOKIE_NAME = "redirectUrl";
     public static final String MODE_PARAM_COOKIE_NAME = "mode";
     private static final int COOKIE_EXPIRE_SECONDS = 180;
 
