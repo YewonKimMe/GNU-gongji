0c6e6298e123451ab1ebb9d5ee089a50d2d1741a
YewonKimMe <kimyewon030428@gmial.com>
2024-09-22 12:57:13 +0900

    response write direct

--- src/main/java/site/gnu_gongji/GnuGongji/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/site/gnu_gongji/GnuGongji/security/CustomAuthenticationEntryPoint.java
@@ -4,18 +4,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
+import site.gnu_gongji.GnuGongji.dto.response.FailResultAndMessage;
+import site.gnu_gongji.GnuGongji.dto.response.ResultAndMessage;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
-    //private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     private final HandlerExceptionResolver resolver;
 
     public CustomAuthenticationEntryPoint(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
@@ -24,13 +30,14 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-//        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-//        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-//        response.setCharacterEncoding("utf-8");
-//
-//        ResultAndMessage<String> data = new FailResultAndMessage<>(HttpStatus.UNAUTHORIZED.getReasonPhrase(), "Authentication Failed.");
-//        String responseData = objectMapper.writeValueAsString(data);
-//        response.getWriter().write(responseData);
-        resolver.resolveException(request, response, null, (BadCredentialsException) request.getAttribute("BadCdEx"));
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("utf-8");
+
+        ResultAndMessage<String> data = new FailResultAndMessage<>(HttpStatus.UNAUTHORIZED.getReasonPhrase(), "Authentication Failed.");
+        String responseData = objectMapper.writeValueAsString(data);
+        response.getWriter().write(responseData);
+//        resolver.resolveException(request, response, null, (BadCredentialsException) request.getAttribute("BadCdEx"));
     }
 }
