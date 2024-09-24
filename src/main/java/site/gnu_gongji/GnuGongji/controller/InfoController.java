0ad61876ebffe2202edb833332f908a1995aab3a
YewonKimMe <kimyewon030428@gmial.com>
2024-09-24 20:52:05 +0900

    feat: InfoController, getDepartmentList

---
+++ src/main/java/site/gnu_gongji/GnuGongji/controller/InfoController.java
@@ -0,0 +1,38 @@
+package site.gnu_gongji.GnuGongji.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import site.gnu_gongji.GnuGongji.dto.response.ResultAndMessage;
+import site.gnu_gongji.GnuGongji.dto.response.SuccessResultAndMessage;
+import site.gnu_gongji.GnuGongji.entity.Department;
+import site.gnu_gongji.GnuGongji.service.DepartmentService;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@Tag(name = "Info", description = "정보 관련 엔드포인트")
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/info")
+public class InfoController {
+
+    private final DepartmentService departmentService;
+
+    // 구독 가능 학과 리스트 획득
+    @Operation(summary = "구독 가능 학과 리스트", description = "구독 가능 학과 리스트 획득 API")
+    @GetMapping("/departments")
+    public ResponseEntity<ResultAndMessage> getDepartmentList() {
+        List<Department> result = departmentService.getAllDepartmentNoticeInfo();
+        return ResponseEntity.ok()
+                .body(new SuccessResultAndMessage(HttpStatus.OK.getReasonPhrase(), result));
+    }
+
+    // 학과 또는 단과대? 검색 기능
+}
