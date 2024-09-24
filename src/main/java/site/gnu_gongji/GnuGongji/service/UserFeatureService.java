374a55f510ad7413d7adf43347c2369b63db5d9a
YewonKimMe <kimyewon030428@gmial.com>
2024-09-24 20:45:51 +0900

    feat: "이메일 변경, 구독 추가, 구독 삭제"
    updateUserEmail, addUserSubDepartment, deleteSubscription 추가

---
+++ src/main/java/site/gnu_gongji/GnuGongji/service/UserFeatureService.java
@@ -0,0 +1,101 @@
+package site.gnu_gongji.GnuGongji.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site.gnu_gongji.GnuGongji.dto.EmailDto;
+import site.gnu_gongji.GnuGongji.entity.User;
+import site.gnu_gongji.GnuGongji.entity.UserSub;
+import site.gnu_gongji.GnuGongji.exception.DepartmentNotExistException;
+import site.gnu_gongji.GnuGongji.exception.NotExistKeyException;
+import site.gnu_gongji.GnuGongji.exception.SubscriptionLimitReachedException;
+import site.gnu_gongji.GnuGongji.exception.UserNotExistException;
+import site.gnu_gongji.GnuGongji.repository.UserFeatureRepository;
+
+import java.util.Optional;
+
+@Transactional
+@RequiredArgsConstructor
+@Slf4j
+@Service
+public class UserFeatureService {
+
+    private final DepartmentService departmentService;
+
+    private final UserFeatureRepository userFeatureRepository;
+
+    // 이메일 변경
+    public void updateUserEmail(final String userOAuth2Id, final EmailDto emailDto) {
+
+        User findUser = userFeatureRepository.findUserByOauth2Id(userOAuth2Id)
+                .orElseThrow(() -> new UserNotExistException("해당 ID로 조회된 유저가 없습니다."));
+
+        findUser.setUserEmail(emailDto.getEmail());
+    }
+
+    // 유저 공지사항 구독 추가
+    public void addUserSubDepartment(final String userOAuth2Id, final Long departmentId) {
+
+        // 유효한 부서 여부 확인
+        checkDepartment(departmentId);
+
+        // 유저 조회
+        Optional<User> findUserOpt = userFeatureRepository.findUserByOauth2Id(userOAuth2Id);
+
+        if (findUserOpt.isEmpty()) {
+            throw new UserNotExistException("해당 ID로 조회된 유저가 없습니다.");
+        }
+
+        User findUser = findUserOpt.get();
+
+        int userSubSize = findUser.getSubList().size();
+
+        if (userSubSize >= 3) {
+            throw new SubscriptionLimitReachedException("공지사항 구독은 최대 2개 까지만 가능합니다.\n현재 구독중인 갯수는 " + userSubSize + " 입니다.");
+        }
+        UserSub userSub = new UserSub();
+
+        userSub.setDepartmentId(departmentId);
+        userSub.setUser(findUser);
+
+        findUser.getSubList().add(userSub);
+    }
+
+    // 구독중인 유저 공지사항 삭제
+    public void deleteSubscription(final String userOAuth2Id, final Long departmentId) {
+
+        // 유효한 여부 확인
+        checkDepartment(departmentId);
+
+        // 유저 조회
+        Optional<User> findUserOpt = userFeatureRepository.findUserByOauth2Id(userOAuth2Id);
+
+        // 조회 결과가 없는 경우
+        if (findUserOpt.isEmpty()) {
+            throw new UserNotExistException("해당 ID로 조회된 유저가 없습니다.");
+        }
+
+        // OAuth2Id 로 조회된 유저
+        User findUser = findUserOpt.get();
+
+        UserSub findUserSub = findUserOpt.get().getSubList()
+                .stream()
+                .filter(userSub -> userSub.getDepartmentId().equals(departmentId))
+                .findFirst()
+                .orElseThrow(() -> new NotExistKeyException("해당 ID로 조회된 구독 정보가 없습니다."));
+
+        // UserSubList 에서 해당 userSub 제거
+        findUser.getSubList().remove(findUserSub);
+
+        // 양방향 관계 정리
+        findUserSub.setUser(null);
+    }
+
+    private void checkDepartment(Long departmentId) {
+        // Department 조회
+        if (!departmentService.checkDepartmentExistByDId(departmentId)) {
+            throw new DepartmentNotExistException("해당 ID로 검색된 학과/부서가 없습니다.");
+        }
+    }
+}
