8f650a2ab0ae8ed44b7301b2614d5951821384d3
YewonKimMe <kimyewon030428@gmial.com>
2024-09-25 15:08:52 +0900

    feat: 사용자 정의 예외 핸들러 추가
    DepartmentNotExistException, DuplicateSubscribeException, NotExistKeyException,
    SubscriptionLimitReachedException, UserNotExistException 핸들링

--- src/main/java/site/gnu_gongji/GnuGongji/controller/ExceptionAdvice.java
+++ src/main/java/site/gnu_gongji/GnuGongji/controller/ExceptionAdvice.java
@@ -8,25 +8,42 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import site.gnu_gongji.GnuGongji.dto.response.FailResultAndMessage;
 import site.gnu_gongji.GnuGongji.dto.response.ResultAndMessage;
+import site.gnu_gongji.GnuGongji.exception.*;
 
-@RestControllerAdvice
+
 @Slf4j
+@RestControllerAdvice
 public class ExceptionAdvice {
 
     // JWT Filter 예외 처리
     @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<ResultAndMessage> handleBadCredentialException(BadCredentialsException e) {
+    public ResponseEntity<ResultAndMessage<String>> handleBadCredentialException(BadCredentialsException e) {
 
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
-                .body(new FailResultAndMessage(HttpStatus.UNAUTHORIZED.getReasonPhrase(), e.getMessage()));
+                .body(new FailResultAndMessage<>(HttpStatus.UNAUTHORIZED.getReasonPhrase(), e.getMessage()));
+    }
+
+    // 사용자 정의 예외 핸들러
+    @ExceptionHandler({
+            DepartmentNotExistException.class,
+            DuplicateSubscribeException.class,
+            NotExistKeyException.class,
+            SubscriptionLimitReachedException.class,
+            UserNotExistException.class
+    })
+    public ResponseEntity<ResultAndMessage<String>> handleMultiException(Exception e) {
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new FailResultAndMessage<>(HttpStatus.BAD_REQUEST.getReasonPhrase(), e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ResultAndMessage> handleException(Exception e) {
+    public ResponseEntity<ResultAndMessage<String>> handleException(Exception e) {
 
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new FailResultAndMessage(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), "unknown error"));
+                .body(new FailResultAndMessage<>(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), "서버에 오류가 발생했습니다."));
     }
 }
