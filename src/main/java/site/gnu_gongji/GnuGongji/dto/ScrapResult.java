1c2370232af9fbaeafd8bb3e936b2d9a7e21082c
YewonKimMe <kimyewon030428@gmial.com>
2024-09-26 13:34:32 +0900

    feat: "ScrapResult 추가"

---
+++ src/main/java/site/gnu_gongji/GnuGongji/dto/ScrapResult.java
@@ -0,0 +1,23 @@
+package site.gnu_gongji.GnuGongji.dto;
+
+
+import lombok.*;
+
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ScrapResult {
+
+    private Long departmentId;
+
+    private String departmentName;
+
+    private String title;
+
+    private String date;
+
+    private String noticeLink;
+}
