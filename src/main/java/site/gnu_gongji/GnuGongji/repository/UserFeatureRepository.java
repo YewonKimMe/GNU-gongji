a3e4a8b45b185e9b4e3e51bec5e3dc80ae8a675f
YewonKimMe <kimyewon030428@gmial.com>
2024-10-01 00:36:15 +0900

    feat: add findDepartmentsByUserOauth2Id

--- src/main/java/site/gnu_gongji/GnuGongji/repository/UserFeatureRepository.java
+++ src/main/java/site/gnu_gongji/GnuGongji/repository/UserFeatureRepository.java
@@ -1,10 +1,14 @@
 package site.gnu_gongji.GnuGongji.repository;
 
+import site.gnu_gongji.GnuGongji.dto.DepartmentDto;
 import site.gnu_gongji.GnuGongji.entity.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserFeatureRepository {
 
     Optional<User> findUserByOauth2Id(String oauth2Id);
+
+    List<DepartmentDto> findDepartmentsByUserOauth2Id(String oauth2Id);
 }
