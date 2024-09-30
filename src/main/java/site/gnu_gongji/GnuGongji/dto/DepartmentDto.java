46a4b45bef1d4fd0492377e94be9a201e0ff63ad
YewonKimMe <kimyewon030428@gmial.com>
2024-10-01 00:19:24 +0900

    feat: add DepartmentDto

---
+++ src/main/java/site/gnu_gongji/GnuGongji/dto/DepartmentDto.java
@@ -0,0 +1,20 @@
+package site.gnu_gongji.GnuGongji.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DepartmentDto {
+    private Long departmentId;
+    private String departmentKo;
+    private String departmentEng;
+
+    public DepartmentDto(Long departmentId, String departmentKo, String departmentEng) {
+        this.departmentId = departmentId;
+        this.departmentKo = departmentKo;
+        this.departmentEng = departmentEng;
+    }
+
+    // Getters and Setters
+}
