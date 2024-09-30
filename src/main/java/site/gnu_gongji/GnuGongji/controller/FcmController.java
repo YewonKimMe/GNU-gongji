ecfb10fbe112a0f1d0fdf04905b80080ea1f520d
YewonKimMe <kimyewon030428@gmial.com>
2024-09-30 12:01:04 +0900

    feat: delete token

--- src/main/java/site/gnu_gongji/GnuGongji/controller/FcmController.java
+++ src/main/java/site/gnu_gongji/GnuGongji/controller/FcmController.java
@@ -5,10 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import site.gnu_gongji.GnuGongji.dto.FcmTokenDto;
 import site.gnu_gongji.GnuGongji.dto.response.ResultAndMessage;
 import site.gnu_gongji.GnuGongji.dto.response.SuccessResultAndMessage;
@@ -23,14 +20,27 @@ public class FcmController {
     private final UserManageService userManageService;
 
     @PostMapping("/token")
-    public ResponseEntity<ResultAndMessage> getUserIdentifyToken(@RequestBody FcmTokenDto fcmTokenDto, Authentication authentication) {
+    public ResponseEntity<ResultAndMessage> enrollFcmToken(@RequestBody FcmTokenDto fcmTokenDto, Authentication authentication) {
 
         log.debug("[token 등록 요청] userId={}", authentication.getName());
+        log.info("[token 등록 요청] userId={}", authentication.getName());
 
-        userManageService.updateUserToken(authentication.getName(), fcmTokenDto.getToken());
+        userManageService.updateUserFcmToken(authentication.getName(), fcmTokenDto.getToken());
 
         return ResponseEntity.ok()
                 .body(new SuccessResultAndMessage<>(HttpStatus.OK.getReasonPhrase(), authentication.getName() + " 계정의 알림 설정 등록이 완료되었습니다."));
+    }
+
+    @DeleteMapping("/token")
+    public ResponseEntity<ResultAndMessage> deleteFcmToken(Authentication authentication) {
+
+        log.debug("[token 삭제 요청] userId={}", authentication.getName());
+        log.info("[token 삭제 요청] userId={}", authentication.getName());
+
+        userManageService.updateUserFcmToken(authentication.getName(), null);
+
+        return ResponseEntity.ok()
+                .body(new SuccessResultAndMessage<>(HttpStatus.OK.getReasonPhrase(), authentication.getName() + " 계정의 알림 설정이 해지되었습니다."));
     }
 
 }
