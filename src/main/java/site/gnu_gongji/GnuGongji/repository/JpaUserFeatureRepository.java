187defc9f9aa0e4913f422a8fd57fe2c0e1a44b6
YewonKimMe <kimyewon030428@gmial.com>
2024-10-01 00:20:38 +0900

    feat: add findDepartmentsByUserOauth2Id
    유저가 구독중인 학과 요약 정보만 획득하는 메소드 추가

--- src/main/java/site/gnu_gongji/GnuGongji/repository/JpaUserFeatureRepository.java
+++ src/main/java/site/gnu_gongji/GnuGongji/repository/JpaUserFeatureRepository.java
@@ -3,12 +3,20 @@ package site.gnu_gongji.GnuGongji.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import site.gnu_gongji.GnuGongji.dto.DepartmentDto;
 import site.gnu_gongji.GnuGongji.entity.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface JpaUserFeatureRepository extends UserFeatureRepository, JpaRepository<User, Long> {
 
     @Query("SELECT u from User u WHERE u.oauth2Id = :oauth2Id")
     Optional<User> findUserByOauth2Id(@Param("oauth2Id") String oauth2Id);
+
+    @Query("SELECT new site.gnu_gongji.GnuGongji.dto.DepartmentDto(d.departmentId, d.departmentKo, d.departmentEng) FROM Department d " +
+            "JOIN UserSub us ON us.departmentId = d.departmentId " +
+            "JOIN us.user u " +
+            "WHERE u.oauth2Id = :oauth2Id")
+    List<DepartmentDto> findDepartmentsByUserOauth2Id(@Param("oauth2Id") String oauth2Id);
 }
