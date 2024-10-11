daef9da3f052e7fb77407a6d2c89f4d8a4067054
YewonKimMe <kimyewon030428@gmial.com>
2024-10-11 10:26:10 +0900

    feat: logging, remove throw

--- src/main/java/site/gnu_gongji/GnuGongji/service/NotificationService.java
+++ src/main/java/site/gnu_gongji/GnuGongji/service/NotificationService.java
@@ -58,7 +58,6 @@ public class NotificationService {
                                     fcmService.sendMessage(fcmMessageDto);
                                 } catch (IOException e) {
                                     log.error("[FCM Message Send Exception], cause={}", e.getMessage());
-                                    throw new RuntimeException(e);
                                 }
                             }
                         });
