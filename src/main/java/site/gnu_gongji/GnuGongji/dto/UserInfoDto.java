9710d8ca8cf2501846f9e16080266f30effbdf11
YewonKimMe <kimyewon030428@gmial.com>
2024-10-16 14:55:09 +0900

    feat: add UserInfoDto

---
+++ src/main/java/site/gnu_gongji/GnuGongji/dto/UserInfoDto.java
@@ -0,0 +1,24 @@
+package site.gnu_gongji.GnuGongji.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserInfoDto {
+
+    private String oauth2Id;
+
+    private String email;
+
+    private String oauth2Provider;
+
+    private String createDate;
+
+    private boolean isPushMessagingAgreed;
+
+}
