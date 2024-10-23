6fbc4a964e59bc72cae795478a45ca92df4abbb7
YewonKimMe <kimyewon030428@gmial.com>
2024-10-23 16:25:06 +0900

    feat: add DeviceUtil

---
+++ src/main/java/site/gnu_gongji/GnuGongji/tool/DeviceUtil.java
@@ -0,0 +1,23 @@
+package site.gnu_gongji.GnuGongji.tool;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import site.gnu_gongji.GnuGongji.security.oauth2.enums.Device;
+
+@Slf4j
+public class DeviceUtil {
+
+    public static Device getDeviceInfo(HttpServletRequest request) {
+        String userAgent = request.getHeader("User-Agent").toLowerCase();
+
+        log.debug(userAgent);
+        if (userAgent.contains("mobi")) {
+            return Device.MOBILE;
+        } else if (userAgent.contains("tablet") || userAgent.contains("ipad")) {
+            return Device.TABLET;
+        } else if (userAgent.contains("windows") || userAgent.contains("macintosh") || userAgent.contains("linux")){
+            return Device.PC;
+        }
+        throw new RuntimeException("Device Not Match");
+    }
+}
