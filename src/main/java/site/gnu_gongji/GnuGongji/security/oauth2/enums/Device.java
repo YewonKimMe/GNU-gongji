616a784526bf8279168aba3effef52a91c1b4623
YewonKimMe <kimyewon030428@gmial.com>
2024-10-23 16:24:57 +0900

    feat: add Device

---
+++ src/main/java/site/gnu_gongji/GnuGongji/security/oauth2/enums/Device.java
@@ -0,0 +1,24 @@
+package site.gnu_gongji.GnuGongji.security.oauth2.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Device {
+
+    MOBILE("MOBILE"),
+    TABLET("TABLET"),
+    PC("PC");
+
+    private final String device;
+
+    public static Device getDevice(String value) {
+        for (Device device : values()) {
+            if (device.getDevice().equals(value)) {
+                return device;
+            }
+        }
+        throw new RuntimeException("Device Not Match");
+    }
+}
