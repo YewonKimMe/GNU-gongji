f7367d7a23803fa9733cd04e4e79fa61fbc976d4
kim-yewon <kimyewon030428@gmail.com>
2024-11-08 23:47:22 +0900

    feat: add UserMemoNotificationRepository

---
+++ src/main/java/site/gnu_gongji/GnuGongji/repository/UserMemoNotificationRepository.java
@@ -0,0 +1,23 @@
+package site.gnu_gongji.GnuGongji.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import site.gnu_gongji.GnuGongji.entity.UserMemoNotification;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserMemoNotificationRepository extends JpaRepository<UserMemoNotification, Long> {
+
+    @Query("select umn from UserMemoNotification umn where umn.userId = :userId")
+    List<UserMemoNotification> getUserMemoNotificationByUserId(@Param("userId") String userId);
+
+    @Query("select umn from UserMemoNotification umn WHERE umn.userId = :userId AND umn.id = :id")
+    Optional<UserMemoNotification> findByUserId(@Param("userId") String userId, @Param("id") Long id);
+
+    @Modifying
+    @Query("delete from UserMemoNotification umn where umn.id = :id AND umn.userId = :userId")
+    void deleteUserMemoNotificationByIdAndUserId(@Param("userId") String userId, @Param("id") Long id);
+}
