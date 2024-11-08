cf2b32accab15500850363f0dfdc4d81839bbbd4
kim-yewon <kimyewon030428@gmail.com>
2024-11-08 23:43:49 +0900

    feat: MemoNotExistException

---
+++ src/main/java/site/gnu_gongji/GnuGongji/exception/MemoNotExistException.java
@@ -0,0 +1,7 @@
+package site.gnu_gongji.GnuGongji.exception;
+
+public class MemoNotExistException extends RuntimeException {
+    public MemoNotExistException(String message) {
+        super(message);
+    }
+}
