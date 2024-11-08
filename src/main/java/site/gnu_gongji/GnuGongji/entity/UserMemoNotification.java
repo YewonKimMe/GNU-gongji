40451b358d5cacc9cf7dc2a9b89b47f1b79b0cf7
kim-yewon <kimyewon030428@gmail.com>
2024-11-08 23:47:01 +0900

    feat: add entity UserMemoNotification

---
+++ src/main/java/site/gnu_gongji/GnuGongji/entity/UserMemoNotification.java
@@ -0,0 +1,33 @@
+package site.gnu_gongji.GnuGongji.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@Getter
+@Setter
+@Table(name = "user_memo_notification")
+public class UserMemoNotification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userId; // oauth2
+
+    private String departmentTitle;
+
+    private String notificationTitle;
+
+    private String link;
+
+    private String time;
+
+    private byte[] encryptedMemo;
+
+
+}
