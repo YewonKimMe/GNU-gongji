f26a713d87d911786eb4546dd245aef534e79057
kim-yewon <kimyewon030428@gmail.com>
2024-11-09 12:23:24 +0900

    feat: add notificationDate, uuid

--- src/main/java/site/gnu_gongji/GnuGongji/dto/FcmNotificationDto.java
+++ src/main/java/site/gnu_gongji/GnuGongji/dto/FcmNotificationDto.java
@@ -3,6 +3,7 @@ package site.gnu_gongji.GnuGongji.dto;
 import lombok.*;
 
 @Getter
+@Setter
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FcmNotificationDto {
@@ -13,13 +14,19 @@ public class FcmNotificationDto {
 
     String body;
 
+    String notificationDate;
+
     String link;
 
+    String uuid;
+
     @Builder(toBuilder = true)
-    public FcmNotificationDto(String token, String title, String body, String link) {
+    public FcmNotificationDto(String token, String title, String body, String notificationDate, String link, String uuid) {
         this.token = token;
         this.title = title;
         this.body = body;
+        this.notificationDate = notificationDate;
         this.link = link;
+        this.uuid = uuid;
     }
 }
