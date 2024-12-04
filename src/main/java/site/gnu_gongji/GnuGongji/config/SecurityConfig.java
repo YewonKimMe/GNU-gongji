d2c5d90a00357d0e020ebb11f3d3326abeef5257
kim-yewon <kimyewon030428@gmail.com>
2024-12-04 14:46:06 +0900

    feat: add aws test path

--- src/main/java/site/gnu_gongji/GnuGongji/config/SecurityConfig.java
+++ src/main/java/site/gnu_gongji/GnuGongji/config/SecurityConfig.java
@@ -81,7 +81,7 @@ public class SecurityConfig {
                                 .requestMatchers("/api/v1/admin/**", "/api/v1/test/**", "/actuator/**").hasRole("ADMIN")
                                 .requestMatchers("/api/v1/info/**").hasRole("USER")
                                 .requestMatchers("/api/v1/firebase/**").hasRole("USER")
-                                .requestMatchers("/oauth2/**", "/login/**").permitAll()
+                                .requestMatchers("/oauth2/**", "/login/**", "/api/v1/aws/**").permitAll()
                                 .anyRequest().denyAll()
                 )
                 .oauth2Login(configurer -> configurer.
