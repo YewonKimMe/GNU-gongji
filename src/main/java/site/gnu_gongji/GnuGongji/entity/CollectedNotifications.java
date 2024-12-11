e003846e4aeea7a85687e71097c1df32f01f364d
kim-yewon <kimyewon030428@gmail.com>
2024-12-11 19:04:19 +0900

    feat: add CollectedNotifications

---
+++ src/main/java/site/gnu_gongji/GnuGongji/entity/CollectedNotifications.java
@@ -0,0 +1,41 @@
+package site.gnu_gongji.GnuGongji.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "collected_notification")
+public class CollectedNotifications {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long departmentId;
+
+    private String departmentName;
+
+    private String notiTitle;
+
+    private String dateTime;
+
+    private String link;
+
+    @Column(name = "created_time")
+    private Timestamp createTime;
+
+    @Builder
+    public CollectedNotifications(Long id, Long departmentId, String departmentName, String notiTitle, String dateTime, String link) {
+        this.id = id;
+        this.departmentId = departmentId;
+        this.departmentName = departmentName;
+        this.notiTitle = notiTitle;
+        this.dateTime = dateTime;
+        this.link = link;
+    }
+}
