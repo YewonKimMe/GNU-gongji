ab5165266bf334fe3a32f809bb10a5c3d2ae9ae1
kim-yewon <kimyewon030428@gmail.com>
2024-12-11 19:05:22 +0900

    feat: add DepartmentNoticeInfoJDBCRepository

--- src/main/java/site/gnu_gongji/GnuGongji/controller/InfoController.java
+++ src/main/java/site/gnu_gongji/GnuGongji/controller/InfoController.java
@@ -4,15 +4,18 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import site.gnu_gongji.GnuGongji.dto.DepartmentDto;
 import site.gnu_gongji.GnuGongji.dto.response.ResultAndMessage;
 import site.gnu_gongji.GnuGongji.dto.response.SuccessResultAndMessage;
+import site.gnu_gongji.GnuGongji.entity.CollectedNotifications;
 import site.gnu_gongji.GnuGongji.service.DepartmentService;
 
 import java.util.List;
@@ -38,4 +41,26 @@ public class InfoController {
     }
 
     // 학과 또는 단과대? 검색 기능
+
+    @GetMapping("/notifications/{departmentId}")
+    public ResponseEntity<PagedModel<EntityModel<CollectedNotifications>>> getCollectedNotification(@PathVariable("departmentId") Long departmentId,
+                                                                                           @RequestParam(name = "page", defaultValue = "0") String page,
+                                                                                           @RequestParam(name = "size", defaultValue = "50") String size,
+                                                                                           @RequestParam(name = "query", required = false) String notificationTitleQuery,
+                                                                                           PagedResourcesAssembler<CollectedNotifications> assembler
+                                                            ) {
+        Integer newPage = Integer.parseInt(page) - 1;
+        if (newPage < 0) {
+            newPage = 0;
+        }
+        log.debug("newPage={}", newPage);
+        Integer newSize = Integer.parseInt(size);
+
+        Page<CollectedNotifications> find = departmentService.getAllCollectedNotifications(newPage, newSize, departmentId, notificationTitleQuery);
+
+        PagedModel<EntityModel<CollectedNotifications>> model = assembler.toModel(find);
+
+        return ResponseEntity.ok()
+                .body(model);
+    }
 }
