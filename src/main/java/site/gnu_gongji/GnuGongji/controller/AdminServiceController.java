7d615251d1233f4e6a0b735a2b928cb79c4d64d1
kim-yewon <kimyewon030428@gmail.com>
2024-12-30 16:10:11 +0900

    fix: add addSubscribeTopicTest, sendTopicMessagingTest, updateSubLimitCountTest

--- src/main/java/site/gnu_gongji/GnuGongji/controller/AdminServiceController.java
+++ src/main/java/site/gnu_gongji/GnuGongji/controller/AdminServiceController.java
@@ -19,6 +19,8 @@ import site.gnu_gongji.GnuGongji.dto.ScrapNotification;
 import site.gnu_gongji.GnuGongji.dto.response.ResultAndMessage;
 import site.gnu_gongji.GnuGongji.dto.response.SuccessResultAndMessage;
 import site.gnu_gongji.GnuGongji.entity.Department;
+import site.gnu_gongji.GnuGongji.enums.Topic;
+import site.gnu_gongji.GnuGongji.repository.AdminRepository;
 import site.gnu_gongji.GnuGongji.service.*;
 
 import java.util.List;
@@ -30,6 +32,11 @@ import java.util.UUID;
 @Tag(name = "Admin", description = "관리자 기능 관련 엔드포인트")
 @RequestMapping("/api/v1/admin")
 public class AdminServiceController {
+
+    @Value("${mycustom.sub-limit}")
+    private int subLimit;
+
+    private final AdminRepository adminRepository;
 
     private final NoticeExcelParser noticeExcelParser;
 
@@ -74,6 +81,50 @@ public class AdminServiceController {
 
     }
 
+    @PostMapping("/firebase-topic-subscribe-test")
+    public ResponseEntity<ResultAndMessage> addSubscribeTopic(@RequestBody TokenDto tokenDto) {
+
+        String topic = Topic.ONLY_TEST_TOPIC_PATH.getPath();
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(topic)
+                .append("으로 [")
+                .append(tokenDto.getToken())
+                .append("] 토큰이 ");
+
+        if (tokenDto.isOption()) {
+            fcmService.subscribeTopic(List.of(tokenDto.getToken()), topic);
+            sb.append("구독 등록");
+        } else {
+            fcmService.unSubscribeTopic(List.of(tokenDto.getToken()), topic);
+            sb.append("구독 해제");
+        }
+        sb.append("됨");
+
+        return ResponseEntity.ok()
+                .body(new SuccessResultAndMessage<>(HttpStatus.OK.getReasonPhrase(), sb.toString()));
+    }
+
+    @PostMapping("/firebase-topic-messaging-test")
+    public ResponseEntity<ResultAndMessage> sendTopicMessaging() {
+
+        fcmService.sendMessageByTopic("[테스트] 경상국립대 장학공지", "[테스트] ★★ 2025학년도 1학기 국가장학금 1차 신청 안내 ★★ (2024.11.15)", "https://www.gnu.ac.kr/main/na/ntt/selectNttInfo.do?mi=1376&bbsId=1075&nttSn=2259007",
+                "2024.11.15", Topic.ONLY_TEST_TOPIC_PATH.getPath(), UUID.randomUUID().toString());
+
+        return ResponseEntity.ok()
+                .body(new SuccessResultAndMessage<>(HttpStatus.OK.getReasonPhrase(), Topic.ONLY_TEST_TOPIC_PATH + " 토픽으로 전송 완료"));
+    }
+
+    @PostMapping("/update-sub-limit-count")
+    public ResponseEntity<ResultAndMessage> updateSubLimitCount(@RequestBody SubLimitDto subLimitDto) {
+
+        adminRepository.updateSubscribeLimit(subLimitDto.getSubLimit());
+
+        return ResponseEntity.ok()
+                .body(new SuccessResultAndMessage<>(HttpStatus.OK.getReasonPhrase(), "모든 유저의 구독 제한 갯수를 " + subLimitDto.getSubLimit() + " 으로 업데이트 완료"));
+    }
+
     @PostMapping("/slack-message-send")
     public ResponseEntity<ResultAndMessage> sendSlackMessage(@RequestBody SlackMessageDto messageDto) {
 
@@ -86,6 +137,10 @@ public class AdminServiceController {
     @PostMapping("/sqs-test")
     public ResponseEntity<ResultAndMessage> sendToSqs(@RequestBody ScrapNotification scrapNotification) {
 
+        String uuid = UUID.randomUUID().toString();
+
+        scrapNotification.setMessageId(uuid);
+        scrapNotification.getScrapResult().setUuid(uuid);
         awsSqsSender.sendScrapNotification(scrapNotification);
 
         return ResponseEntity.ok()
@@ -96,5 +151,18 @@ public class AdminServiceController {
     @Setter
     private static class SlackMessageDto {
         private String text;
+    }
+
+    @Getter
+    @Setter
+    public static class TokenDto {
+        private String token;
+        private boolean option;
+    }
+
+    @Getter
+    @Setter
+    public static class SubLimitDto {
+        private Integer subLimit;
     }
 }
