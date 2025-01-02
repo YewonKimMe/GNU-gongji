06c0a054312d21ba21cf191368c843184f4ef716
kim-yewon <kimyewon030428@gmail.com>
2025-01-02 16:38:49 +0900

    fix: add setter

--- src/main/java/site/gnu_gongji/GnuGongji/dto/ScrapNotification.java
+++ src/main/java/site/gnu_gongji/GnuGongji/dto/ScrapNotification.java
@@ -2,11 +2,13 @@ package site.gnu_gongji.GnuGongji.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class ScrapNotification {
 
