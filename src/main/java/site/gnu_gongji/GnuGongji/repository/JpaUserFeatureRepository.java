4f9c988d4005bfcae1b3a8341d649d5502df1229
kim-yewon <kimyewon030428@gmail.com>
2025-01-05 21:32:06 +0900

    fix: change to UserSubInfoDto

--- src/main/java/site/gnu_gongji/GnuGongji/repository/JpaUserFeatureRepository.java
+++ src/main/java/site/gnu_gongji/GnuGongji/repository/JpaUserFeatureRepository.java
@@ -3,7 +3,7 @@ package site.gnu_gongji.GnuGongji.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import site.gnu_gongji.GnuGongji.dto.DepartmentDto;
+import site.gnu_gongji.GnuGongji.dto.UserSubInfoDto;
 import site.gnu_gongji.GnuGongji.entity.User;
 
 import java.util.List;
@@ -14,9 +14,9 @@ public interface JpaUserFeatureRepository extends UserFeatureRepository, JpaRepo
     @Query("SELECT u from User u WHERE u.oauth2Id = :oauth2Id")
     Optional<User> findUserByOauth2Id(@Param("oauth2Id") String oauth2Id);
 
-    @Query("SELECT new site.gnu_gongji.GnuGongji.dto.DepartmentDto(d.departmentId, d.departmentKo, d.departmentEng) FROM Department d " +
+    @Query("SELECT new site.gnu_gongji.GnuGongji.dto.UserSubInfoDto(d.departmentId, d.departmentKo, d.departmentEng, us.startAt) FROM Department d " +
             "JOIN UserSub us ON us.departmentId = d.departmentId " +
             "JOIN us.user u " +
             "WHERE u.oauth2Id = :oauth2Id")
-    List<DepartmentDto> findDepartmentsByUserOauth2Id(@Param("oauth2Id") String oauth2Id);
+    List<UserSubInfoDto> findDepartmentsByUserOauth2Id(@Param("oauth2Id") String oauth2Id);
 }
